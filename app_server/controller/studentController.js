221b7980f8331afeb44ac0ea2b2ea8b8e44eab18
Ramazan Öğünç <ramazanogunc@hotmail.com>
2020-12-11 21:31:08 +0300

    Some coding

--- app_server/controller/studentController.js
+++ app_server/controller/studentController.js
@@ -1,11 +1,11 @@
 var config = require('../dbconfig');
 
 module.exports.addQuestions = function(req, res) {
-    var question = req.query.question;
-    var answer = req.query.answer;
-    var branchId = req.query.branchId;
-    var studentId = req.query.studentId;
-    var mentorId = req.query.mentorId;
+    var question = req.body.question;
+    var answer = req.body.answer ;
+    var branchId = req.body.branchId;
+    var studentId = req.body.studentId;
+    var mentorId = req.body.mentorId;
 
     var sql = 'INSERT INTO questions(question, answer, branchId, studentId, mentorId) VALUES (?,?,?,?,?)'
 
@@ -14,5 +14,19 @@ module.exports.addQuestions = function(req, res) {
         res.write('Inserted...');
         res.end();
     });
+
+};
+
+module.exports.getQuestion = function(req, res) {
+    var sql = 'SELECT * FROM questions'
+    try {
+        config.query(sql, function(err, rows) {
+            if (err) throw err;
+            res.json(rows);
+        });
+
+    } catch (err) {
+        throw err
+    }
 
 };
--- app_server/routes/studentRoutes.js
+++ app_server/routes/studentRoutes.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var router = express.Router();
 
-var ctrMentor = require('../controller/studentController');
+var ctrStudent = require('../controller/studentController');
 
-router.post('/add', ctrMentor.addQuestions);
+router.post('/login');
+router.post('/register');
+router.post('/addQuestion', ctrStudent.addQuestions);
+router.get('/getQuestionList', ctrStudent.getQuestion);
+router.get('/:id/getMentorInfo');
+router.get('/:id/getProfileInfo');
+router.post('/:id/updateProfile');
 
 module.exports = router;
