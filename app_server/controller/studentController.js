e2ccdfa04221592b4e90be7905a9379bb5bbb53d
Ramazan Öğünç <ramazanogunc@hotmail.com>
2020-12-11 22:31:12 +0300

    Prepare student routes

--- app_server/controller/studentController.js
+++ app_server/controller/studentController.js
@@ -1,8 +1,14 @@
 var config = require('../dbconfig');
 
-module.exports.addQuestions = function(req, res) {
+/*
+req: 
+question, id
+response: 
+200
+*/
+module.exports.addQuestions = function (req, res) {
     var question = req.body.question;
-    var answer = req.body.answer ;
+    var answer = req.body.answer;
     var branchId = req.body.branchId;
     var studentId = req.body.studentId;
     var mentorId = req.body.mentorId;
@@ -17,10 +23,117 @@ module.exports.addQuestions = function(req, res) {
 
 };
 
-module.exports.getQuestion = function(req, res) {
+/*
+req: 
+id
+response:
+200:
+[
+    {
+        question, answer, tarih(db,ye eklenecek), branş ismi, mentör ad soyad
+    }
+]
+*/
+module.exports.getQuestion = function (req, res) {
     var sql = 'SELECT * FROM questions'
     try {
-        config.query(sql, function(err, rows) {
+        config.query(sql, function (err, rows) {
+            if (err) throw err;
+            res.json(rows);
+        });
+
+    } catch (err) {
+        throw err
+    }
+
+};
+
+/*
+req: username, password
+response: 200 - { kullaniciId }, 403 
+*/
+module.exports.login = function (req, res) {
+    var sql = 'SELECT * FROM questions'
+    try {
+        config.query(sql, function (err, rows) {
+            if (err) throw err;
+            res.json(rows);
+        });
+
+    } catch (err) {
+        throw err
+    }
+
+};
+
+/*
+req: ad, soyad, mail, şifre, alanId
+response: 200
+*/
+module.exports.register = function (req, res) {
+    var sql = 'SELECT * FROM questions'
+    try {
+        config.query(sql, function (err, rows) {
+            if (err) throw err;
+            res.json(rows);
+        });
+
+    } catch (err) {
+        throw err
+    }
+
+};
+
+/*
+request:
+response: 200 ok { ad soyad, mentörAlanAdı, hakkinda }
+*/
+module.exports.getMentorInfo = function (req, res) {
+    var sql = 'SELECT * FROM questions'
+    try {
+        config.query(sql, function (err, rows) {
+            if (err) throw err;
+            res.json(rows);
+        });
+
+    } catch (err) {
+        throw err
+    }
+
+};
+
+/*
+request:
+response: 200 ok { ad, soyad, mail, şifre, alanId }
+*/
+module.exports.getProfileInfo = function (req, res) {
+    var sql = 'SELECT * FROM questions'
+    try {
+        config.query(sql, function (err, rows) {
+            if (err) throw err;
+            res.json(rows);
+        });
+
+    } catch (err) {
+        throw err
+    }
+
+};
+
+/*
+request:ad, soyad, mail, şifre, alanId
+response: 200ost('/login', ctrStudent.login);
+router.post('/register', ctrStudent.register);
+router.post('/:id/addQuestion', ctrStudent.addQuestions);
+router.get('/:id/getQuestionList', ctrStudent.getQuestion);
+router.get('/:id/getMentorInfo', ctrStudent.getMentorInfo);
+router.get('/:id/getProfileInfo', ctrStudent.getProfileInfo);
+router.po
+*/
+module.exports.updateProfile = function (req, res) {
+    var sql = 'SELECT * FROM questions'
+    try {
+        config.query(sql, function (err, rows) {
             if (err) throw err;
             res.json(rows);
         });
--- app_server/routes/studentRoutes.js
+++ app_server/routes/studentRoutes.js
@@ -4,12 +4,12 @@ var router = express.Router();
 
 var ctrStudent = require('../controller/studentController');
 
-router.post('/login');
-router.post('/register');
-router.post('/addQuestion', ctrStudent.addQuestions);
-router.get('/getQuestionList', ctrStudent.getQuestion);
-router.get('/:id/getMentorInfo');
-router.get('/:id/getProfileInfo');
-router.post('/:id/updateProfile');
+router.post('/login', ctrStudent.login);
+router.post('/register', ctrStudent.register);
+router.post('/:id/addQuestion', ctrStudent.addQuestions);
+router.get('/:id/getQuestionList', ctrStudent.getQuestion);
+router.get('/:id/getMentorInfo', ctrStudent.getMentorInfo);
+router.get('/:id/getProfileInfo', ctrStudent.getProfileInfo);
+router.post('/:id/updateProfile', ctrStudent.updateProfile);
 
 module.exports = router;
