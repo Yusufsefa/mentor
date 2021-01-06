21a10e90898f32880d9aa91c9d012580fe9a689c
Alperen <alperenoz1464@gmail.com>
2021-01-06 19:46:44 +0300

    "changes in adminController and adminRoutes"

--- app_server/controller/adminController.js
+++ app_server/controller/adminController.js
@@ -1,8 +1,28 @@
 var config = require('../dbconfig');
 
-module.exports.getAllStudent = function(req, res) {
+module.exports.login = function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+
     try {
-        config.query('select * from students', function(err, rows) {
+        config.query('select * from admins WHERE email=? AND password=?', [email, password], function (err, rows) {
+            if (err) throw err;
+            if (rows.length == 1)
+                res.json(rows[0])
+            else
+                res.status(404)
+            res.end()
+
+        });
+
+    } catch (err) {
+        throw err
+    }
+};
+
+module.exports.getAllStudent = function (req, res) {
+    try {
+        config.query('select * from students', function (err, rows) {
             if (err) throw err;
             res.json(rows);
         });
@@ -12,9 +32,9 @@ module.exports.getAllStudent = function(req, res) {
     }
 };
 
-module.exports.getAllMentor = function(req, res) {
+module.exports.getAllMentor = function (req, res) {
     try {
-        config.query('select * from mentors', function(err, rows) {
+        config.query('select * from mentors', function (err, rows) {
             if (err) throw err;
             res.json(rows);
         });
--- app_server/routes/adminRoutes.js
+++ app_server/routes/adminRoutes.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 var ctrAdmin = require('../controller/adminController');
 
+router.post('/login', ctrAdmin.login);
 router.get('/allStudent', ctrAdmin.getAllStudent);
 router.get('/allMentor', ctrAdmin.getAllMentor);
 
